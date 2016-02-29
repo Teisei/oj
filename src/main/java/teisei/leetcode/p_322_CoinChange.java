8181dc173dec80a9d79f1ad530871cb998ff0c93
Teisei <teisei.din@gmail.com>
2016-02-29 21:36:42 +0800

    2016.2.29-1

---
+++ src/main/java/teisei/leetcode/p_322_CoinChange.java
@@ -0,0 +1,54 @@
+package teisei.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Created by Teisei on 2016/2/27.
+ */
+public class p_322_CoinChange {
+    public static void main(String[] args) {
+        p_322_CoinChange test = new p_322_CoinChange();
+        test.run();
+    }
+
+    public void run() {
+        int[] x = new int[]{1, 2, 5};
+        x = new int[]{186,419,83,408};
+        int amount = 6249;
+        System.out.println(coinChange(x, amount));
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int dp[][] = new int[coins.length][amount + 1];
+        for(int i=0;i<=amount;i++)
+            dp[0][i] = i % coins[0] == 0 ? i / coins[0]: -1;
+        for(int i=1;i<coins.length;i++) {
+            int x = coins[i];
+            for (int j = 0; j <= amount; j++) {
+//                int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE, c = Integer.MAX_VALUE;
+                if (j - x < 0){
+                    dp[i][j] = dp[i - 1][j];
+                }
+                else{
+                    dp[i][j] = -1;
+                    int a = dp[i][j - x] == -1 ? Integer.MAX_VALUE : dp[i][j - x] + 1;
+                    int b = dp[i - 1][j] == -1 ? Integer.MAX_VALUE : dp[i - 1][j];
+                    dp[i][j] = Math.min(a, b);
+                    if (dp[i][j] == Integer.MAX_VALUE) {
+                        dp[i][j] = -1;
+                    }
+                }
+            }
+        }
+        boolean flag = false;
+        int res = Integer.MAX_VALUE;
+        for(int i=0;i<coins.length;i++) {
+            if (dp[i][amount] != -1) {
+                res = Math.min(res, dp[i][amount]);
+                flag = true;
+            }
+        }
+        return flag ? res : -1;
+    }
+}
---
+++ src/main/java/teisei/leetcode/p_70_ClimbingStairs.java
@@ -0,0 +1,27 @@
+package teisei.leetcode;
+
+/**
+ * Created by Teisei on 2016/2/28.
+ */
+public class p_70_ClimbingStairs {
+    public static void main(String[] args) {
+        p_70_ClimbingStairs test = new p_70_ClimbingStairs();
+        test.run();
+    }
+
+    public void run() {
+        int n = 0;
+        System.out.println(climbStairs(n));
+    }
+
+    public int climbStairs(int n) {
+        if(n==0) return 0;
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
