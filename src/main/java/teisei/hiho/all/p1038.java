8929ab707eb05341c5c806eca5a5b7474e979120
Teisei <teisei.din@gmail.com>
2016-04-05 15:47:36 +0800

    2016.4.5-1

---
+++ src/main/java/teisei/hiho/all/p1038.java
@@ -0,0 +1,43 @@
+package teisei.hiho.all;
+
+import java.util.Scanner;
+
+/**
+ * Created by Teisei on 2016/4/5.
+ */
+public class p1038 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        //freopen("in.txt","r",stdin);
+        int N, M;
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        int[] need = new int[N];
+        int[] value = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            need[i] = sc.nextInt();
+            value[i] = sc.nextInt();
+        }
+        int[] dp = new int[M + 1];
+        for (int j = 0; j <= M; j++)
+            dp[j] = j < need[0] ? 0 : value[0];
+        for (int i = 1; i < N; i++) {
+            for (int j = M; j >= 0; j--) {
+                if (j >= need[i])
+                    dp[j] = Math.max(dp[j], dp[j - need[i]] + value[i]);
+            }
+        }
+        int v_max = 0;
+        for (int j = 0; j <= M; j++)
+            v_max = Math.max(v_max, dp[j]);
+        System.out.println(v_max);
+    }
+}
---
+++ src/main/java/teisei/hiho/ms_2014_intern/p1.java
@@ -0,0 +1,77 @@
+package teisei.hiho.ms_2014_intern;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Teisei on 2016/4/5.
+ */
+public class p1 {
+
+    final static char c_init = '/';
+    final static int i_0 = (int) '0';
+    final static int i_9 = (int) '9';
+    final static int i_a = (int) 'a';
+    final static int i_z = (int) 'z';
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static boolean isLegal(char x) {
+        int i_x = (int) x;
+        return ((i_x >= i_0 && i_x <= i_9) || (i_x >= i_a && i_x <= i_z));
+    }
+
+    public static String process(String line) {
+        char[] array = line.toCharArray();
+        Arrays.sort(array);
+
+        List<Character> list = new ArrayList<>();
+        for (char x : array) {
+            list.add(x);
+        }
+
+        int len = array.length;
+        String total_tmp = "";
+
+
+        while (list.size() > 0) {
+            int i = 0;
+            String tmp = "";
+            char max_x = c_init;
+            for (; i < list.size(); i++) {
+                char x = list.get(i);
+                if (!isLegal(x)) return "<invalid input string>";
+
+                if (max_x < x) {
+                    max_x = x;
+                    tmp += max_x;
+                    list.remove(i);
+                    i--;
+                }
+            }
+            total_tmp += tmp;
+        }
+        return total_tmp;
+    }
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    public static void test1() {
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String res = process(line);
+            System.out.println(res);
+        }
+    }
+
+
+    public static void test2() {
+        String line = "";
+        String res = process(line);
+        System.out.println(res);
+    }
+}
